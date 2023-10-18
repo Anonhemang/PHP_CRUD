dc7e7749e9c18fbaf24c7d1f35945259f11e1906
Anonhemang <83428512+Anonhemang@users.noreply.github.com>
2023-10-18 10:13:45 +0530

    Add files via upload

---
+++ poojainfotech/conn.php
@@ -0,0 +1,17 @@
+<?php 
+$username = "localhost";
+$server = "root";
+$pass = "";
+$dbname = "pooja";
+
+
+$conn = mysqli_connect($username,$server, $pass, $dbname);
+if($conn){
+    //echo "Connected";
+}
+else{
+    echo "error";
+}
+
+
+?>
